83fcf4eebad4f86207445a599f156360cf5e6554
ivaniordanov <ivaniordanov@gmail.com>
2022-11-18 16:29:23 +0200

    Code with examples after discussion

--- fingerprint_parser/parse.py
+++ fingerprint_parser/parse.py
@@ -18,35 +18,23 @@ def write(filename, contents):
     with open(filename, 'w') as f:
         f.write(contents)
 
-def script_features(value, url, element):
+def script_features(value, url, dom_element):
     return 'self_hosted' if value.startswith('http') and value.find(url) > -1 else 'external_host'
 
 def files_iterator(data_dir):
     return ((filename, extract_url(filename)) for filename in glob.glob(data_dir + "*"))
 
-class TagFeatures(object):
-    def __init__(self, handlers):
-        self._handlers = handlers
-
-    def extract(self, url, element):
-        return self._features(url, element, self._handlers.get(element.name, self._default_attributes))
-
-    def _features(self, url, element, extractors):
-        return flatten([extract.features(url, element) for extract in extractors])
-
-    @property
-    def _default_attributes(self):
-        return [SingleValueAttribute('id')]
 
 class Fingerprint(object):
-    def __init__(self, tag_features):
+    def __init__(self, encoder, tag_features):
+        self._encoder = encoder
         self._tag_features = tag_features
 
     def features(self, url, document):
-        return [] if not hasattr(document, 'body') else self.get(url, document.body)
+        return [] if not hasattr(document, 'body') else self.get(url, document)
 
     def get(self, url, element):
-        features = [hash(feature) for feature in self._tag_features.extract(url, element)]
+        features = [self._encoder(feature) for feature in self._tag_features.extract(url, element)]
         children_features = flatten([self.get(url, child) for child in element.children if not isinstance(child, bs4.element.NavigableString)])
         return features + list(children_features)
 
@@ -71,9 +59,25 @@ class SingleValueAttribute(object):
     def _value(self, value, url, element):
         return "{}.{}".format(element.name, value)
 
+class TagFeatures(object):
+    def __init__(self, handlers, default=[SingleValueAttribute('id')]):
+        self._handlers = handlers
+        self._default_attributes = default
 
-finger_print = Fingerprint(TagFeatures({
-    'script': [SingleValueAttribute('src', [script_features]), SingleValueAttribute('id')]
+    def extract(self, url, element):
+        return self._features(url, element, self._handlers.get(element.name, self._default_attributes))
+
+    def _features(self, url, element, extractors):
+        return flatten([extract.features(url, element) for extract in extractors])
+
+
+def identity(x): return x
+def remove_tag(x): return x.split(".", 1)[1]
+#finger_print = Fingerprint(identity, TagFeatures({ 'script': [SingleValueAttribute('src')] }, []))
+#finger_print = Fingerprint(remove_tag, TagFeatures({ 'link': [SingleValueAttribute('href')] }, []))
+finger_print = Fingerprint(hash, TagFeatures({
+    'script': [SingleValueAttribute('src', [script_features])],
+    'link': [SingleValueAttribute('href')]
 }))
 features = {}
 
@@ -81,6 +85,8 @@ data_dir = "../data/html/"
 for filename, url in files_iterator(data_dir):
     try:
         features[url] = finger_print.features(url, bs4.BeautifulSoup(read(filename), "html.parser"))
+        print (features[url])
+        break
     except:
         print (filename)
 
